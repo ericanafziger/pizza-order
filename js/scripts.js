fd9a590069655b1c5b198786b0e913a176e56689
Erica Nafziger <garbererica@gmail.com>
2016-11-04 14:28:14 -0700

    add the add pizza button in the order section - this button adds a new pizza to the users pizzas array. Does not yet display what each pizza contains on the final order page

--- js/scripts.js
+++ js/scripts.js
@@ -27,13 +27,18 @@ Pizza.prototype.cost = function () {
   return sizePrice + crustPrice + toppingsPrice;
 };
 
-var newCustomer = new Customer();
+function reload() {
+  $("#checkbox").prop("checked", false);
+  parseFloat($("#size").val(""));
+  $("#crust").val("");
+}
 
 
 
 
 /////// front end logic /////////////////
 $(document).ready(function(){
+  newCustomer = new Customer ();
 
   $("form#userName").submit(function(event){
     event.preventDefault();
@@ -45,45 +50,50 @@ $(document).ready(function(){
     $(".nameForOrder").text(nameInput);
     $(".emailForOrder").text(emailInput);
     $("#section-one").fadeOut(300);
-    $("#section-number").delay(300).fadeIn(300);
+    $("#section-two").delay(300).fadeIn(300);
 
   }); // end of userName submit
-  $("form#pizzaNumber").submit(function(event){
-    event.preventDefault();
-    var numberInput = $("#number").val();
-    for (i = 0; i < numberInput; i++) {
-      newCustomer.pizzas.push(new Pizza());
-    }
-    console.log(newCustomer);
-    $("#section-number").fadeOut(300);
-    $("#section-two").delay(300).fadeIn(300);
-    alert(numberInput);
-  });
+
+  newCustomer.pizzas.push(new Pizza());
+  var pizzaNumber = (newCustomer.pizzas.length);
+  $(".pizzaNumber").text(pizzaNumber);
 
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
-    newCustomer.pizzas[0].toppings = [];
-    $("input:checkbox[name=toppings]:checked").each(function(){
-      newCustomer.pizzas[0].toppings.push($(this).val());
-    });
-    newCustomer.pizzas[0].crust = $("#crust").val();
-    newCustomer.pizzas[0].size = parseFloat($("#size").val());
-    var cost = newCustomer.pizzas[0].cost();
 
-    if (newCustomer.pizzas[0].crust && newCustomer.pizzas[0].size && newCustomer.pizzas[0].toppings.length > 0) {
-      $(".price").text(cost);
-      $(".crustPrice").text(crustPrice.toFixed(2));
-      $(".toppingsPrice").text(toppingsPrice.toFixed(2));
-      $(".sizePrice").text(newCustomer.pizzas[0].size);
-      $(".btn-default").fadeIn();
-      $(".re").text("Re-");
-    } else {
-      alert("Hold up! You haven't finished your pizza yet.");
-    }
-    console.log(newCustomer);
+
+      newCustomer.pizzas[pizzaNumber - 1].toppings = [];
+      $("input:checkbox[name=toppings]:checked").each(function(){
+        newCustomer.pizzas[pizzaNumber - 1].toppings.push($(this).val());
+      });
+      newCustomer.pizzas[pizzaNumber - 1].crust = $("#crust").val();
+      newCustomer.pizzas[pizzaNumber - 1].size = parseFloat($("#size").val());
+      var cost = newCustomer.pizzas[pizzaNumber - 1].cost();
+
+      if (newCustomer.pizzas[pizzaNumber - 1].crust && newCustomer.pizzas[pizzaNumber - 1].size && newCustomer.pizzas[0].toppings.length > 0) {
+        $(".price").text(cost);
+        $(".crustPrice").text(crustPrice.toFixed(2));
+        $(".toppingsPrice").text(toppingsPrice.toFixed(2));
+        $(".sizePrice").text(newCustomer.pizzas[pizzaNumber - 1].size);
+        $(".btn-default").fadeIn();
+        $(".btn-success").fadeIn();
+        $(".re").text("Re-");
+      } else {
+        alert("Hold up! You haven't finished your pizza yet.");
+      }
+
+      console.log(newCustomer);
   }); // end of pizzaForm submit
+  $(".btn-success").click(function(){
+    newCustomer.pizzas.push(new Pizza());
+    pizzaNumber = (newCustomer.pizzas.length);
+    $(".pizzaNumber").text(pizzaNumber);
+    console.log(newCustomer);
+
+  });
+
   $(".btn-default").click(function(){
-    $("#section-two").hide();
+    $("#section-two").fadeOut();
     $("#section-three").fadeIn();
 
   });
