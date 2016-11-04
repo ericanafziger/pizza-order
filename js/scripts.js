10f746e522d24f112ec0eb883c0a62cf7c35c8ee
Erica Nafziger <garbererica@gmail.com>
2016-11-04 08:36:29 -0700

    checkboxes will push to toppings array for the customer object

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,42 @@
-var customer = function(name, email, toppings, crust, size) {
+var Customer = function(name, email, toppings, crust, size) {
   this.name = name;
   this.email = email;
   this.toppings = [];
+  this.crust = crust;
   this.size = size;
 }
 
-
+var newCustomer = new Customer();
 
 
 
 
 $(document).ready(function(){
+    debugger;
 
+  $("form#userName").submit(function(event){
+    event.preventDefault();
+
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    newCustomer.name = nameInput;
+    newCustomer.email = emailInput;
+    console.log(newCustomer);
+    $("#section-one").fadeOut(300);
+    $("#section-two").delay(300).fadeIn(300);
+
+  });
+
+  $("form#pizzaForm").submit(function(event){
+    event.preventDefault();
+
+    $("input:checkbox[name=toppings]:checked").each(function(){
+      newCustomer.toppings.push($(this).val());
+      console.log(newCustomer);
+
+    });
+
+  });
 
 
 });
