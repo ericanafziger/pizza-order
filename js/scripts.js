78d771473af76339b843f9acf5bd107da569a3b9
Erica Nafziger <garbererica@gmail.com>
2016-11-04 13:34:49 -0700

    add functionality so user can select more than one pizza to order. App will currently update array for pizza one based on selections; there is not currently a way to choose toppings for next pizza

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,22 @@
 /////// back end logic /////////////////
-var Customer = function(name, email, toppings, crust, size) {
+var Customer = function(name, email, pizzas) {
   this.name = name;
   this.email = email;
-  this.toppings = [];
+  this.pizzas = [];
+  // this.toppings = [];
+  // this.crust = crust;
+  // this.size = size;
+}
+var Pizza = function(toppings, crust, size) {
+  this.toppings = toppings;
   this.crust = crust;
   this.size = size;
 }
+
 var crustPrice;
 var toppingsPrice;
 
-Customer.prototype.cost = function () {
+Pizza.prototype.cost = function () {
   toppingsPrice = this.toppings.length * 0.75;
   if (this.crust === "stuffed" || this.crust === "deep dish") {
     crustPrice = 1.99;
@@ -38,25 +45,36 @@ $(document).ready(function(){
     $(".nameForOrder").text(nameInput);
     $(".emailForOrder").text(emailInput);
     $("#section-one").fadeOut(300);
-    $("#section-two").delay(300).fadeIn(300);
+    $("#section-number").delay(300).fadeIn(300);
 
   }); // end of userName submit
+  $("form#pizzaNumber").submit(function(event){
+    event.preventDefault();
+    var numberInput = $("#number").val();
+    for (i = 0; i < numberInput; i++) {
+      newCustomer.pizzas.push(new Pizza());
+    }
+    console.log(newCustomer);
+    $("#section-number").fadeOut(300);
+    $("#section-two").delay(300).fadeIn(300);
+    alert(numberInput);
+  });
 
   $("form#pizzaForm").submit(function(event){
     event.preventDefault();
-    newCustomer.toppings = [];
+    newCustomer.pizzas[0].toppings = [];
     $("input:checkbox[name=toppings]:checked").each(function(){
-      newCustomer.toppings.push($(this).val());
+      newCustomer.pizzas[0].toppings.push($(this).val());
     });
-    newCustomer.crust = $("#crust").val();
-    newCustomer.size = parseFloat($("#size").val());
-    var cost = newCustomer.cost();
+    newCustomer.pizzas[0].crust = $("#crust").val();
+    newCustomer.pizzas[0].size = parseFloat($("#size").val());
+    var cost = newCustomer.pizzas[0].cost();
 
-    if (newCustomer.crust && newCustomer.size && newCustomer.toppings.length > 0) {
+    if (newCustomer.pizzas[0].crust && newCustomer.pizzas[0].size && newCustomer.pizzas[0].toppings.length > 0) {
       $(".price").text(cost);
       $(".crustPrice").text(crustPrice.toFixed(2));
       $(".toppingsPrice").text(toppingsPrice.toFixed(2));
-      $(".sizePrice").text(newCustomer.size);
+      $(".sizePrice").text(newCustomer.pizzas[0].size);
       $(".btn-default").fadeIn();
       $(".re").text("Re-");
     } else {
