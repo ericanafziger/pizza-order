1fdf011b8bcb314db2cd0623865af0b2a5879b88
Erica Nafziger <garbererica@gmail.com>
2016-11-04 08:20:14 -0700

    add inputs, forms, and buttons in html

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+var customer = function(name, email, toppings, crust, size) {
+  this.name = name;
+  this.email = email;
+  this.toppings = [];
+  this.size = size;
+}
+
+
+
+
+
+
+$(document).ready(function(){
+
+
+
+});
